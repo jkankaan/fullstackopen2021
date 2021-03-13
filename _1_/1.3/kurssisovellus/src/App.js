c9cf4948569e92a69a60a7e701a40b1775f49157
jkankaan <juhani.kankaanpaeae@gmail.com>
2021-03-13 19:06:46 +0200

    exercise 1.3 added

---
+++ _1_/1.3/kurssisovellus/src/App.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+  return (
+    <div>
+      <Header course = {course}></Header>
+      <Content part1 ={part1.name} exercises1 = {part1.exercises}
+      part2 ={part2.name} exercises2 = {part2.exercises}
+      part3 ={part3.name} exercises3 = {part3.exercises}></Content>
+      <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}></Total>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+    <Part part = {props.part1} exercises = {props.exercises1}></Part>
+    <Part part = {props.part2} exercises = {props.exercises2}></Part>
+    <Part part = {props.part3} exercises = {props.exercises3}></Part>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+      {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+export default App
