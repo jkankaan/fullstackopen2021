5661e7d6fcb6622bed2924df2d972a57fc4c9c41
jkankaan <juhani.kankaanpaeae@gmail.com>
2021-03-15 18:06:56 +0200

    exercise 1.7 added

---
+++ _1_/1.7/unicafe/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good+1)}>good</button>
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button onClick={() => setBad(bad+1)}>bad</button>
+      <h1>statistics</h1>
+      good {good}<br></br>
+      neutral {neutral}<br></br>
+      bad {bad}<br></br>
+      all {good+neutral+bad}<br></br>
+      average {(good*1+neutral*0+bad*-1)/(good+neutral+bad)}<br></br>
+      positive {(good)/(good+neutral+bad)*100} %
+    </div>
+  )
+}
+
+export default App
