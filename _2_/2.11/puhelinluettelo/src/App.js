7ab7fa70722698f6aacc01c19a6a0b53d957063f
jkankaan <juhani.kankaanpaeae@gmail.com>
2021-04-02 14:20:21 +0300

    exercise 2.11 added

---
+++ _2_/2.11/puhelinluettelo/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+import axios from 'axios'
+
+const App = () => {
+  const [ persons, setPersons] = useState([])
+
+  const [ newName, setNewName ] = useState('')
+
+  const [ newNumber, setNewNumber ] = useState('')
+
+  const [ newFilter, setNewFilter ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons').then(response => {
+        setPersons(response.data)
+      })
+  },[])
+
+  const addNameAndNumber = (event) => {
+    event.preventDefault()
+    const nameAndNumberObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (persons.find(person => person.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+    }
+    else {  
+      setPersons(persons.concat(nameAndNumberObject))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const filtered = persons.filter(person => {
+    return person.name.toLowerCase().includes(newFilter.toLowerCase())
+    })
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <Filter filter = {newFilter} handle = {handleFilterChange}/>
+      <h2>add a new</h2>
+        <PersonForm nameandnumber = {addNameAndNumber} newname = {newName}
+        handlename = {handleNameChange} newnumber = {newNumber}
+        handlenumber = {handleNumberChange}/>
+      <h2>Numbers</h2>
+        <Persons persons = {filtered}/>     
+    </div>
+  )
+
+}
+
+export default App
