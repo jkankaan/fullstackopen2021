f64074ed2c6d02d802f9326d2dc67b55d2196309
Juhani Kankaanpää <juhanikankaanpaa@Juhanis-MBP.home>
2022-04-28 14:42:28 +0300

    directory structure changed

--- _4_/4.2/blogilista/index.js
+++ _4_/4.2/blogilista/index.js
@@ -4,15 +4,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const logger = require('./utils/logger.js')
+const config = require('./utils/config.js')
+const blogsRouter = require('./controllers/blogs.js')
 
 //const mongoUrl = 'mongodb://localhost/bloglist'
 const mongoUrl = process.env.MONGODB_URI
@@ -21,25 +15,9 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
