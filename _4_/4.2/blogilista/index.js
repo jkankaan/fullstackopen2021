00d9a4c072df3ead0a1fff64a5c56ce26ab2a53b
Juhani Kankaanpää <juhanikankaanpaa@Juhanis-MBP.home>
2022-05-03 12:12:05 +0300

    exercise 4.2 finished

--- _4_/4.2/blogilista/index.js
+++ _4_/4.2/blogilista/index.js
@@ -1,21 +1,7 @@
 require('dotenv').config()
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app.js')
 const logger = require('./utils/logger.js')
 const config = require('./utils/config.js')
-const blogsRouter = require('./controllers/blogs.js')
-
-//const mongoUrl = 'mongodb://localhost/bloglist'
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
